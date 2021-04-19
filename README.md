b78115fdbb91b6a114e343b2d054bf5de4b7f8aa
zhuzhuodong <zhuzhuodong@meituan.com>
2021-04-19 17:57:43 +0800

    add readme

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# Demo
+# 示例
 ![avatar](/readme/demo.gif)
 
-# Usages
+# 用法
 
-## Gradle
+## Gradle引入
 
 ### Add the JitPack repository to your build file
 ```
@@ -22,5 +22,65 @@ dependencies {
 }
 ```
 
-## layout
+## 代码使用
 
+### 默认用法
+
+你可以使用定制化程度高的原生Builder。
+```java
+SimpleTipDialog.Builder builder = SimpleTipDialog.Builder.builder()
+    .title("Your title")
+    .mainTip("Main tip message")
+    .subTip("Sub tip message")
+    .leftBtnTxt("Left btn")
+    .rightBtnTxt("Right Btn")
+    .leftClickListener(new SimpleTipDialog.OnClickListener() {
+        @Override
+        public void onClick(SimpleTipDialog dialog) {
+            //your code,do not forget call dismisss();
+            dialog.dismiss();
+        }
+    })
+    .rightClickListener(new SimpleTipDialog.OnClickListener() {
+        @Override
+        public void onClick(SimpleTipDialog dialog) {
+            //your code,do not forget call dismisss();
+            dialog.dismiss();
+        }
+    })
+    .build();
+SimpleTipDialog dialog = new SimpleTipDialog(context, builder);
+dialog.show();
+```
+
+### 常用的对话框样式
+
+我们也提供了一些常见的提示框在<code>BuilderFactory</code>.
+
++ newSucessBuilder: 成功提示;
++ newInfoBuilder: 普通提示;
++ newWarnBuilder: 提示提示;
++ newErrorBuilder: 错误提示;
+
+### Builder支持的所有属性
+
+| 属性                    | 释义                                                          |
+|:-----------------------|:--------------------------------------------------------------|
+| title                  | 对话框标题                                                      |
+| titleColor             | 标题文本字体颜色                                                |
+| mainTip                | 主提示信息                                                      |
+| mainTipColor           | 主提示信息文本字体颜色                                           |
+| subTip                 | 副提示信息（设置为空时不展示，主要用于温馨提示）                     |
+| subTipClor             | 父提示信息文本颜色                                               |
+| showLeftBtn            | 是否展示左边按钮（默认为true，最大支持两个按钮）                    |
+| leftBtnTxt             | 左边按钮文本                                                    |
+| leftBtnBgColor         | 左边按钮背景色                                                  |
+| leftBtnColor           | 左边按钮文本文字字体颜色                                          |
+| leftClickListener      | 左边按钮点击事件（不设置默认dismiss()，设置后需手动调用dissmisss()） |
+| rightBtnTxt            | 右边按钮文本                                                    |
+| rightBtnBgColor        | 右边按钮背景色                                                  |
+| rightBtnColor          | 右边按钮文本文字字体颜色                                          |
+| rightClickListener     | 右边按钮点击事件（不设置默认dismiss()，设置后需手动调用dissmisss()） |
+| showCloseBtn           | 是否展示右上角关闭按钮（默认true）                                |
+| cancelable             | 是否支持返回键关闭Dialog（默认true）                              |
+| canceledOnTouchOutside | 点击除Dialog其他空白处是否关闭Dialog（默认true）                   |
--- app/src/main/java/com/xici/library/androidsimpletipdialog/MainActivity.java
+++ app/src/main/java/com/xici/library/androidsimpletipdialog/MainActivity.java
@@ -1,6 +1,6 @@
 package com.xici.library.androidsimpletipdialog;
 
-import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -11,7 +11,7 @@ import com.xici.library.simpletipdialog.SimpleTipDialog;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Activity activity;
+    Context context;
 
     Button defaultDialog;
     Button successDialog;
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        activity = this;
+        context = this;
         defaultDialog = findViewById(R.id.defaultDialog);
         defaultDialog.setOnClickListener(this);
         successDialog = findViewById(R.id.successDialog);
@@ -40,21 +40,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.defaultDialog:
-                new SimpleTipDialog(activity, SimpleTipDialog.Builder.builder().build()).show();
+                new SimpleTipDialog(context, SimpleTipDialog.Builder.builder().build()).show();
                 break;
             case R.id.successDialog:
-                new SimpleTipDialog(activity, BuilderFactory.newSucessBuilder()).show();
+                new SimpleTipDialog(context, BuilderFactory.newSucessBuilder()).show();
                 break;
             case R.id.infoDialog:
-                new SimpleTipDialog(activity, BuilderFactory.newInfoBuilder()).show();
+                new SimpleTipDialog(context, BuilderFactory.newInfoBuilder()).show();
                 break;
             case R.id.warnDialog:
-                new SimpleTipDialog(activity, BuilderFactory.newWarnBuilder()).show();
+                new SimpleTipDialog(context, BuilderFactory.newWarnBuilder()).show();
                 break;
             case R.id.errorDialog:
-                new SimpleTipDialog(activity, BuilderFactory.newErrorBuilder()).show();
+                new SimpleTipDialog(context, BuilderFactory.newErrorBuilder()).show();
                 break;
         }
+    }
+
+    private void test() {
+        SimpleTipDialog.Builder builder = SimpleTipDialog.Builder.builder()
+                .title("Your title")
+                .mainTip("Main tip message")
+                .subTip("Sub tip message")
+                .leftBtnTxt("Left btn")
+                .rightBtnTxt("Right Btn")
+                .leftClickListener(new SimpleTipDialog.OnClickListener() {
+                    @Override
+                    public void onClick(SimpleTipDialog dialog) {
+                        //your code,do not forget call dismisss();
+                        dialog.dismiss();
+                    }
+                })
+                .rightClickListener(new SimpleTipDialog.OnClickListener() {
+                    @Override
+                    public void onClick(SimpleTipDialog dialog) {
+                        //your code,do not forget call dismisss();
+                        dialog.dismiss();
+                    }
+                })
+                .build();
+        SimpleTipDialog dialog = new SimpleTipDialog(context, builder);
+        dialog.show();
     }
 
 }
